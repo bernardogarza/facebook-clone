f21e5f18b6ad121f2d221c348614f1874755fe7a
Kalib Hackin <nagui08@hotmail.com>
2019-09-03 15:32:43 -0500

    I forgot to save a file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Testing User model' do
+    it 'Validating ' do
+      
+    end
+  end
 end
