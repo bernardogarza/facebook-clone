b2cb15b95235096e7a168c1f4233a8a57c721613
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-03 17:00:44 -0500

    Working on the tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
         password {'123456'}
         password_confirmation {'123456'}
     end
+
+    factory :user1, class: User do
+        first_name {'John'}
+        last_name {'Doe'}
+        email {'john@doe.com'}
+        password {'123456'}
+        password_confirmation {'123456'}
+    end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -3,9 +3,34 @@ require 'rails_helper'
 RSpec.feature "Users", type: :feature do
   context 'Tesing Users Controllers' do
     let(:user) { create(:user) }
-    scenario 'It has to show all users' do
+    let(:user1) {create(:user1)}
+    scenario 'It has to ask you to sign in or log in' do
       visit users_path
-      expect(page).to have_content('assa')
+      expect(page).to have_content('You need to sign in or sign up before continuing')
     end
+
+    # before do
+    #   visit users_path
+    #   within('form') do
+    #     fill_in 'Email', with: 'john@doe.com'
+    #     fill_in 'Password', with: '123456'
+    #   end
+    #   click_button 'Log in'
+    # end
+    # scenario 'Log in' do
+    #   visit new_user_session_path
+    #   expect(page).to have_content('Foo Bar')
+    # end
+
+      scenario 'Log in' do
+        visit new_user_session_path
+        User.create(first_name: 'Foo', last_name: 'Bar', email: 'foo@bar.com', )
+        within('form') do
+          fill_in 'Email', with: 'foo@bar.com'
+          fill_in 'Password', with: '123456'
+          click_button 'Log in'
+        end
+        expect(page).to have_content('')
+      end
   end
 end
