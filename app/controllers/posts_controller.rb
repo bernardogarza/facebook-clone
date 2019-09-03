f20f446958893d33d7325ea80dc9aadac6051a36
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-03 13:42:32 -0500

    Working in the edit post part

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,32 @@
 class PostsController < ApplicationController
     def create
-        post = current_user.posts.build(post_params)
-        if post.save
+        @post = current_user.posts.build(post_params)
+        if @post.save
             redirect_back(fallback_location: root_path)
         else
-            flash[:error] = post.errors.full_messages
+            flash[:error] = @post.errors.full_messages
             redirect_back(fallback_location: root_path)
         end
     end
 
     def destroy
-        post = Post.find(params[:id])
-		post.destroy
+        @post = Post.find(params[:id])
+		@post.destroy
 		redirect_back(fallback_location: root_path)
     end
 
     def edit
-        
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to root_url
+        else
+            render 'edit'
+        end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
-  resources :posts, only: %i[create destroy edit]
+  resources :posts, only: %i[create destroy edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
