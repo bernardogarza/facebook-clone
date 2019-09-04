ddc6a1256d0b8d4faffe521b1aa0c3fe306bde52
Kalib Hackin <nagui08@hotmail.com>
2019-09-04 17:51:14 -0500

    working on friendship views and controllers

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,9 @@ class FriendshipsController < ApplicationController
     end
 
     def create
-    
+        user = User.find(params[:user_id])
+        friend_request = current_user.friendships.build(friend: user).save
+        redirect_back(fallback_location: root_path)
     end
 
     def destroy
