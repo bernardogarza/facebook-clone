02a72bf9b67228e49715ef828762090590ffb5b5
Kalib Hackin <nagui08@hotmail.com>
2019-09-05 12:04:12 -0500

    working on authenticate user and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root 'homes#index'
+    resources :posts, only: %i[create destroy edit update]
+    resources :users, only: %i[index show]
+  end
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
-  authenticated :user do
-    root 'homes#index'
-  end
-
   devise_for :users
-  resources :posts, only: %i[create destroy edit update]
-  resources :users, only: %i[index show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
