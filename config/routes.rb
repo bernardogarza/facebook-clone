e4d755abae93d5ba43cac97efe61c9d0c4a76a2d
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-05 12:32:06 -0500

    working on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  authenticated :user do
+  # authenticated :user do
+  #   root 'homes#index'
+  #   resources :posts, only: %i[create destroy edit update]
+  #   resources :users, only: %i[index show]
+  # end
+  # devise_scope :user do
+  #   root to: 'devise/sessions#new'
+  # end
+  # devise_for :users
+  authenticate :user do
     root 'homes#index'
-    resources :posts, only: %i[create destroy edit update]
-    resources :users, only: %i[index show]
   end
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
   devise_for :users
+  resources :posts, only: %i[create destroy edit update]
+  resources :users, only: %i[index show]
 end
