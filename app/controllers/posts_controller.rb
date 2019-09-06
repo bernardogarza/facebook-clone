74811bd5d4835dc828d375f84a048810b3129a2b
Kalib Hackin <nagui08@hotmail.com>
2019-09-06 11:23:36 -0500

    Fixed one isse

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!
+    before_action :find_post, only: %i[destroy edit update]
 
     def create
         @post = current_user.posts.build(post_params)
@@ -13,19 +14,15 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
 		@post.destroy
         redirect_back(fallback_location: root_path)
         flash[:notice] = "Post successfully deleted"
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
-
         if @post.update(post_params)
             redirect_to root_url
             flash[:notice] = "Post successfully edited"
@@ -38,5 +35,9 @@ class PostsController < ApplicationController
     private
         def post_params
             params.require(:post).permit(:body)
+        end
+
+        def find_post
+            @post = Post.find(params[:id])
         end
 end
