8f2d31c72e5d10867946b8edc5905259a22dac2f
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-10 16:29:18 -0500

    Working on omniauth tests

--- spec/features/omniauth_spec.rb
+++ spec/features/omniauth_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
+OmniAuth.config.test_mode = true
 
 RSpec.feature "Omniauths", type: :feature do
   context 'Testing Omniauth Log in' do
     before do
-      OmniAuth.config.add_mock(:facebook, {:uid => '12345'})
-      visit '/auth/facebook'
+      OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+        :provider => 'facebook',
+        :uid => '123545'
+      })
+    end
+    before do
+      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
+      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
     end
 
     it "sets a session variable to the OmniAuth auth hash" do
-      session['uid'].should == '12345'
+      User['uid'].expect == '123545'
     end
   end
 end
