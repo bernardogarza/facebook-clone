268f84066ff9145e9b37ea97f67585c217211f3e
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-10 15:32:07 -0500

    Working on the omniauth authenitcation

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,17 @@ class User < ApplicationRecord
     friends.include?(user)
   end
 
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.first_name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
   private
 
   def downcase_email
