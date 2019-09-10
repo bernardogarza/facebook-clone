86b8173fd14850d5c98545884ff4676f7cbbbf78
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-10 16:55:37 -0500

    Fixed rubocop errors

--- spec/features/omniauth_spec.rb
+++ spec/features/omniauth_spec.rb
@@ -1,21 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 OmniAuth.config.test_mode = true
 
-OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
+OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new(
   provider: 'facebook',
   info: {
     first_name: 'first',
     last_name: 'last',
     email: 'foo@bar.com'
   }
-})
+)
 
 feature 'OmniAuth Signup' do
-
   before do
-    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user]
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
   end
 
   scenario 'can sign up via facebook' do
