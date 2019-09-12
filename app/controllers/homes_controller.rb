b9faf35808e078069a14fc8c2bb26153eaf87c73
Kalib Hackin <nagui08@hotmail.com>
2019-09-12 11:55:12 -0500

    fixed some issues

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,6 @@ class HomesController < ApplicationController
   def index
     if user_signed_in?
       @posts = current_user.posts.order('created_at DESC')
-      @friends_posts = Post.all
     else
       redirect_to new_user_session_path
     end
