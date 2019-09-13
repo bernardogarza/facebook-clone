c366d3de0bf5aacdf0882837ef6e7e4213243bb7
bernardogarza <bernardo.g.landa@gmail.com>
2019-09-13 14:00:01 -0500

    Minor changes on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,10 +51,16 @@ class User < ApplicationRecord
     friends.include?(user)
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end  
+
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.password_confirmation = user.password
